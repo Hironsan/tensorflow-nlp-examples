87fadd1bf21857b687fc4aa9dde3dd29c1c8bb05
Hironsan <light.tree.1.13@gmail.com>
2017-06-22 09:39:53 +0900

    change cost function
    to sparse_softmax_cross_entropy_with_logits

--- language_modeling/w_rnn.py
+++ language_modeling/w_rnn.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 DATA_PATH = 'belling_the_cat.txt'
 LEARNING_RATE = 0.001
-N_EPOCKS = 50000
+N_EPOCHS = 50000
 SKIP_STEP = 1000
 NUM_STEPS = 3
 HIDDEN_SIZE = 512
@@ -36,12 +36,12 @@ def create_rnn(seq, n_hidden):
 
 def create_model(seq, temp, vocab_size, n_hidden=HIDDEN_SIZE):
     outputs, state = create_rnn(seq, n_hidden)
-    logits = tf.contrib.layers.fully_connected(outputs, vocab_size, None)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=temp))
+    logits = tf.contrib.layers.fully_connected(outputs[-1], vocab_size, None)
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=temp))
     return loss, state
 
 
-def training(seq, temp, loss, state, optimizer, words, dictionary, reverse_dictionary, vocab_size):
+def training(seq, temp, loss, state, optimizer, words, dictionary):
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
@@ -49,18 +49,15 @@ def training(seq, temp, loss, state, optimizer, words, dictionary, reverse_dicti
         offset = random.randint(0, NUM_STEPS + 1)
         end_offset = NUM_STEPS + 1
         loss_total = 0
-        for step in range(N_EPOCKS):
+        for step in range(N_EPOCHS):
             if offset > (len(words) - end_offset):
                 offset = random.randint(0, NUM_STEPS + 1)
 
             symbols_in_keys = [[dictionary[words[i]] for i in range(offset, offset + NUM_STEPS)]]
+            symbols_out_onehot = [dictionary[words[offset + NUM_STEPS]]]
 
-            symbols_out_onehot = np.zeros([vocab_size], dtype=float)
-            symbols_out_onehot[dictionary[words[offset + NUM_STEPS]]] = 1.0
-            symbols_out_onehot = np.reshape(symbols_out_onehot, [1, -1])
-
-            _, batch_loss = sess.run([optimizer, loss], feed_dict={seq: symbols_in_keys, temp: symbols_out_onehot})
-            loss_total += batch_loss
+            _, loss_batch = sess.run([optimizer, loss], feed_dict={seq: symbols_in_keys, temp: symbols_out_onehot})
+            loss_total += loss_batch
             if (step + 1) % SKIP_STEP == 0:
                 print('Iter= {}, Average Loss= {:.6f}'.format(step + 1, loss_total / SKIP_STEP))
                 loss_total = 0
@@ -72,10 +69,10 @@ def main():
     dictionary, reverse_dictionary = build_vocabulary(words)
     vocab_size = len(dictionary)
     x = tf.placeholder(shape=[None, NUM_STEPS], dtype=tf.float32)
-    y = tf.placeholder(shape=[None, vocab_size], dtype=tf.float32)
+    y = tf.placeholder(shape=[None], dtype=tf.int32)
     loss, state = create_model(x, y, vocab_size)
     optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
-    training(x, y, loss, state, optimizer, words, dictionary, reverse_dictionary, vocab_size)
+    training(x, y, loss, state, optimizer, words, dictionary)
 
 
 if __name__ == '__main__':
