34670d14eb8a4fac51994363b0dd353e291f65da
Hironsan <light.tree.1.13@gmail.com>
2017-09-07 15:59:14 +0900

    add named-entity recognition example

---
+++ named_entity_recognition/tests/trainer_test.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+
+from ..config import ModelConfig, TrainingConfig
+from ..train import Trainer
+from ..data_loader import load_data_and_labels, load_word_embeddings, batch_iter
+from ..preprocess import prepare_preprocessor
+from ..model import NERModel
+
+
+class TestTrainer(unittest.TestCase):
+
+    def test_train(self):
+        DATA_ROOT = os.path.join(os.path.dirname(__file__), '../data/ner')
+        SAVE_ROOT = os.path.join(os.path.dirname(__file__), '../models')  # trained model
+        LOG_ROOT = os.path.join(os.path.dirname(__file__), '../logs')     # checkpoint, tensorboard
+        embedding_path = os.path.join(os.path.dirname(__file__), '../data/glove.6B/glove.6B.100d.txt')
+
+        model_config = ModelConfig()
+        training_config = TrainingConfig()
+
+        train_path = os.path.join(DATA_ROOT, 'train.txt')
+        valid_path = os.path.join(DATA_ROOT, 'valid.txt')
+        x_train, y_train = load_data_and_labels(train_path)
+        x_valid, y_valid = load_data_and_labels(valid_path)
+
+        p = prepare_preprocessor(x_train, y_train)
+        model_config.num_words = len(p.vocab_word)
+        model_config.num_chars = len(p.vocab_char)
+        model_config.num_tags  = len(p.vocab_tag)
+        embeddings = load_word_embeddings(p.vocab_word, embedding_path, model_config.word_embedding_size)
+
+        train_steps, train_batches = batch_iter(x_train, y_train, training_config.batch_size, preprocessor=p)
+        valid_steps, valid_batches = batch_iter(x_valid, y_valid, training_config.batch_size, preprocessor=p)
+
+        trainer = Trainer(NERModel,
+                          model_config,
+                          training_config,
+                          checkpoint_path=LOG_ROOT,
+                          save_path=SAVE_ROOT,
+                          embeddings=embeddings)
+        trainer.train(train_steps, train_batches, valid_steps, valid_batches)
+
+        p.save(os.path.join(SAVE_ROOT, 'preprocessor.pkl'))
---
+++ named_entity_recognition/train.py
@@ -0,0 +1,111 @@
+import tensorflow as tf
+
+
+class Trainer(object):
+
+    def __init__(self,
+                 model,
+                 model_config,
+                 training_config,
+                 checkpoint_path='',
+                 save_path='',
+                 tensorboard=True,
+                 embeddings=None
+                 ):
+
+        self.model_config = model_config
+        self.training_config = training_config
+        self.checkpoint_path = checkpoint_path
+        self.save_path = save_path
+        self.tensorboard = tensorboard
+
+        self.word_ids = tf.placeholder(tf.int32, shape=[None, None], name='word_ids')
+        self.sequence_lengths = tf.placeholder(tf.int32, shape=[None], name='sequence_lengths')
+        self.char_ids = tf.placeholder(tf.int32, shape=[None, None, None], name='char_ids')
+        self.word_lengths = tf.placeholder(tf.int32, shape=[None, None], name='word_lengths')
+        self.labels = tf.placeholder(tf.int32, shape=[None, None], name='labels')
+        self.dropout = tf.placeholder(dtype=tf.float32, shape=[], name='dropout')
+        self.lr = tf.placeholder(dtype=tf.float32, shape=[], name='lr')
+
+        self.model = model(model_config,
+                           embeddings,
+                           self.word_ids,
+                           self.sequence_lengths,
+                           self.char_ids,
+                           self.word_lengths,
+                           self.labels,
+                           self.dropout)
+        self.model.build()
+
+    def train(self, train_steps, train_batches, valid_steps=None, valid_batches=None):
+        train_op = self.get_train_op()
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            for epoch in range(self.training_config.max_epoch):
+                self.training_config.learning_rate *= self.training_config.lr_decay
+                for i, (data, labels) in enumerate(train_batches):
+                    if i == train_steps:
+                        break
+                    fd, _ = self.get_feed_dict(data, labels,
+                                               self.training_config.learning_rate,
+                                               self.training_config.dropout)
+                    _, train_loss = sess.run([train_op, self.model.loss], feed_dict=fd)
+
+    def get_feed_dict(self, data, labels=None, lr=None, dropout=None):
+        """
+        Builds a feed dictionary.
+        """
+        feed = {}
+
+        if self.model_config.char_feature:
+            word_ids, char_ids, sequence_lengths, word_lengths = data
+            feed[self.char_ids] = char_ids
+            feed[self.word_lengths] = word_lengths
+        else:
+            word_ids, sequence_lengths = data
+
+        feed[self.word_ids] = word_ids
+        feed[self.sequence_lengths] = sequence_lengths
+
+        if labels is not None:
+            feed[self.labels] = labels
+
+        if lr:
+            feed[self.lr] = lr
+
+        if dropout:
+            feed[self.dropout] = dropout
+
+        return feed, sequence_lengths
+
+    def get_train_op(self):
+        """Gets train_op.
+        """
+        with tf.variable_scope('train_step'):
+            # sgd method
+            if self.training_config.lr_method == 'adam':
+                optimizer = tf.train.AdamOptimizer(self.lr)
+            elif self.training_config.lr_method == 'adagrad':
+                optimizer = tf.train.AdagradOptimizer(self.lr)
+            elif self.training_config.lr_method == 'sgd':
+                optimizer = tf.train.GradientDescentOptimizer(self.lr)
+            elif self.training_config.lr_method == 'rmsprop':
+                optimizer = tf.train.RMSPropOptimizer(self.lr)
+            else:
+                raise NotImplementedError('Unknown train op {}'.format(self.training_config.lr_method))
+
+            # gradient clipping if config.clip_value is positive
+            if self.training_config.clip_value > 0:
+                gradients, variables = zip(*optimizer.compute_gradients(self.model.loss))
+                gradients, global_norm = tf.clip_by_global_norm(gradients, self.training_config.clip_value)
+                train_op = optimizer.apply_gradients(zip(gradients, variables))
+            else:
+                train_op = optimizer.minimize(self.model.loss)
+
+            return train_op
+
+    def add_summary(self):
+        # tensorboard stuff
+        self.merged = tf.summary.merge_all()
+        self.file_writer = tf.summary.FileWriter(self.checkpoint_path, self.sess.graph)
