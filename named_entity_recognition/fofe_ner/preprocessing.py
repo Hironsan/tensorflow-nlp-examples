c64733188315469079545b16c55bce2788b143ff
Hironsan <hiroki.nakayama.py@gmail.com>
2018-09-10 06:27:41 +0900

    Update preprocessing

--- named_entity_recognition/fofe_ner/preprocessing.py
+++ named_entity_recognition/fofe_ner/preprocessing.py
@@ -8,12 +8,9 @@ from sklearn.externals import joblib
 from keras.utils.np_utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
 from seqeval.metrics.sequence_labeling import get_entities
+from nltk import ngrams
 
 from .utils import Vocabulary
-
-UNK = '<UNK>'
-PAD = '<PAD>'
-NONE = '<NONE>'
 
 
 class IndexTransformer(BaseEstimator, TransformerMixin):
@@ -82,30 +79,40 @@ class IndexTransformer(BaseEstimator, TransformerMixin):
             features: document id matrix.
             y: label id matrix.
         """
-        word_ids = [self._word_vocab.doc2id(doc) for doc in X]
-        word_ids = pad_sequences(word_ids, padding='post')
+        mentions = []
+        mentions_char = []
+        left_contexts = []
+        right_contexts = []
+        outputs = []
 
-        if self._use_char:
-            char_ids = [[self._char_vocab.doc2id(w) for w in doc] for doc in X]
-            char_ids = pad_nested_sequences(char_ids)
-            features = [word_ids, char_ids]
-        else:
-            features = word_ids
+        word_ids = [self._word_vocab.doc2id(doc) for doc in X]
+        char_ids = [[self._char_vocab.doc2id(w) for w in doc] for doc in X]
+        ngram_indices = []
+        for sent in word_ids:
+            ngrams = self.generate_ngrams(sent, n=4)
+            ngram_indices.append(ngrams)
+            for l, r in ngrams:
+                mentions.append(word_ids[l: r])
+                mentions_char.append(char_ids[l:r])
+                left_contexts.append(word_ids[:l])
+                right_contexts.append(word_ids[r:])
 
         if y is not None:
-            y = [self._label_vocab.doc2id(doc) for doc in y]
-            y = pad_sequences(y, padding='post')
-            y = to_categorical(y, self.label_size).astype(int)
-            # In 2018/06/01, to_categorical is a bit strange.
-            # >>> to_categorical([[1,3]], num_classes=4).shape
-            # (1, 2, 4)
-            # >>> to_categorical([[1]], num_classes=4).shape
-            # (1, 4)
-            # So, I expand dimensions when len(y.shape) == 2.
-            y = y if len(y.shape) == 3 else np.expand_dims(y, axis=0)
-            return features, y
+            for ngram, labels in zip(ngram_indices, y):
+                d = {(begin_offset, end_offset + 1): t for t, begin_offset, end_offset in get_entities(labels)}
+                for l, r in ngram:
+                    if (l, r) in d:
+                        outputs.append(self._label_vocab[d[(l, r)]])
+                    else:
+                        outputs.append(self._label_vocab)
+
+        outputs = np.array(outputs)
+        inputs = [np.array(left_contexts), np.array(mentions), np.array(mentions_char), np.array(right_contexts)]
+
+        if y is not None:
+            return inputs, outputs
         else:
-            return features
+            return inputs
 
     def fit_transform(self, X, y=None, **params):
         """Learn vocabulary and return document id matrix.
@@ -139,6 +146,15 @@ class IndexTransformer(BaseEstimator, TransformerMixin):
 
         return inverse_y
 
+    def generate_ngrams(self, words, n=7):
+        res = []
+        seqlen = len(words)
+        for i in range(1, n + 1):
+            for ngram in ngrams(range(seqlen), i):
+                l, r = ngram[0], ngram[-1] + 1
+                res.append((l, r))
+        return res
+
     @property
     def word_vocab_size(self):
         return len(self._word_vocab)
@@ -150,103 +166,6 @@ class IndexTransformer(BaseEstimator, TransformerMixin):
     @property
     def label_size(self):
         return len(self._label_vocab)
-
-    def save(self, file_path):
-        joblib.dump(self, file_path)
-
-    @classmethod
-    def load(cls, file_path):
-        p = joblib.load(file_path)
-
-        return p
-
-
-class StaticPreprocessor(BaseEstimator, TransformerMixin):
-
-    def __init__(self, lowercase=True, num_norm=True):
-        self._lowercase = lowercase
-        self._num_norm = num_norm
-        self.word_dic = {PAD: 0, UNK: 1}
-        self.char_dic = {PAD: 0, UNK: 1}
-        self.label_dic = {NONE: 0}
-
-    def fit(self, X, y=None):
-        for words in X:
-            for w in words:
-                for c in w:
-                    if c in self.char_dic:
-                        continue
-                    self.char_dic[c] = len(self.char_dic)
-
-                if self._lowercase:
-                    w = w.lower()
-                if w in self.word_dic:
-                    continue
-                self.word_dic[w] = len(self.word_dic)
-
-        for tags in y:
-            for t in tags:
-                t = t.split('-')[-1]
-                if t in self.label_dic:
-                    continue
-                self.label_dic[t] = len(self.label_dic)
-
-        return self
-
-    def transform(self, X, y=None):
-        mentions = []
-        mentions_char = []
-        left_contexts = []
-        right_contexts = []
-        outputs = []
-        for i, words in enumerate(X):
-            char_ids = [self._get_char_ids(w) for w in words]
-            word_ids = [self.word_dic.get(self.process(w), self.word_dic[UNK]) for w in words]
-            ngrams = self.generate_ngrams(word_ids)
-            if y is not None:
-                d = {(begin_offset, end_offset + 1): t for t, begin_offset, end_offset in get_entities(y[i])}
-            for l, r in ngrams:
-                mentions.append(word_ids[l:r])
-                mentions_char.append(char_ids[l:r])
-                left_contexts.append(word_ids[:l])
-                right_contexts.append(word_ids[r:])
-                if y is not None and (l, r) in d:
-                    outputs.append(self.label_dic[d[(l, r)]])
-                else:
-                    outputs.append(self.label_dic[NONE])
-
-        outputs = np.array(outputs)
-
-        inputs = [np.array(left_contexts), np.array(mentions), np.array(mentions_char), np.array(right_contexts)]
-
-        return (inputs, outputs) if y is not None else inputs
-
-    def generate_ngrams(self, words, n=7):
-        res = []
-        leng = len(words)
-        for i in range(leng):
-            for j in range(i + 1, min(i + 1 + n, leng + 1)):
-                res.append((i, j))
-        return res
-
-    def process(self, w):
-        if self._lowercase:
-            w = w.lower()
-        return w
-
-    def _get_char_ids(self, word):
-        return [self.char_dic.get(c, self.char_dic[UNK]) for c in word]
-
-    def fit_transform(self, X, y=None, **fit_params):
-        return self.fit(X, y).transform(X, y)
-
-    def inverse_transform(self, X=None, docs=None):
-        if X is not None:
-            id2char = {i: c for c, i in self.char_dic.items()}
-            return [[id2char[c] for c in sent] for sent in X]
-        id2label = {i: t for t, i in self.label_dic.items()}
-
-        return [[id2label[t] for t in doc] for doc in docs]
 
     def save(self, file_path):
         joblib.dump(self, file_path)
